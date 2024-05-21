4a65df313cefb8b7368185e564e921c526d08128
FraVer203 <113484791+FraVer203@users.noreply.github.com>
2024-05-21 01:54:13 -0600

    Update Profile.jsx

--- src/screen/profile/Profile.jsx
+++ src/screen/profile/Profile.jsx
@@ -2,13 +2,12 @@ import '../welcome/welcome.css';
 
 function Profile({ user }) {
     return (
-        <div className="profile">
+        <div className="container">
             <h2>Perfil de Usuario</h2>
             <p>Nombre: {user.name}</p>
             <p>Apellido: {user.lastName}</p>
             <p>Correo: {user.email}</p>
-            <p>Edad: {user.age}</p>
-            {/* Puedes agregar más detalles según las propiedades del objeto de usuario */}
+            <p>Edad: {user.age}</p
         </div>
     )
 }
